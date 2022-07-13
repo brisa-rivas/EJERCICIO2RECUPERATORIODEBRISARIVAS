74f8f0c71b2dd656b59d35526d2a58e0c0b890e4
Alumno <Alumno@BRII-RIVAS>
2022-07-12 22:57:26 -0300

    Agregar archivos de proyecto.

---
+++ EJERCICIO2RECUPERATORIODEBRISARIVAS/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace EJERCICIO2RECUPERATORIODEBRISARIVAS
+	{
+
+		static void Main(string[] args)
+		{
+			double ax;
+			double bx;
+			double cx;
+			double dy;
+			Console.Write("ingrese el valor de Ax2:");
+			ax = Double.Parse(Console.ReadLine());
+			Console.Write("ingrese el valor de Bx2:");
+			bx = Double.Parse(Console.ReadLine());
+			Console.Write("ingrese el valor de Cx :");
+			cx = Double.Parse(Console.ReadLine());
+			Console.Write("ingrese el valor de Dy:");
+			dy = Double.Parse(Console.ReadLine());
+			if (Math.Pow(ax, 2) != 0 && Math.Pow(bx, 2) == 0)
+			{
+				Console.WriteLine("es una parabola");
+			}
+			if (Math.Pow(ax, 2) == 0 && Math.Pow(bx, 2) != 0)
+			{
+				Console.WriteLine("es una parabola");
+			}
+			if (Math.Pow(ax, 2) > 0 && Math.Pow(bx, 2) > 0)
+			{
+				if (Math.Pow(ax, 2) != Math.Pow(bx, 2))
+				{
+					Console.WriteLine("es una elipse");
+				}
+			}
+			if (Math.Pow(ax, 2) < 0 && Math.Pow(bx, 2) < 0)
+			{
+				if (Math.Pow(bx, 2) != Math.Pow(ax, 2))
+				{
+					Console.WriteLine("es una elipse");
+				}
+			}
+			if (Math.Pow(ax, 2) != 0 && Math.Pow(bx, 2) != 0)
+			{
+				if (Math.Pow(ax, 2) < 0 && Math.Pow(bx, 2) > 0)
+				{
+					if (Math.Pow(ax, 2) != Math.Pow(bx, 2))
+					{
+						Console.WriteLine("es una hiperbola");
+					}
+				}
+			}
+			if (Math.Pow(ax, 2) != 0 && Math.Pow(bx, 2) != 0)
+			{
+				if (Math.Pow(ax, 2) > 0 && Math.Pow(bx, 2) < 0)
+				{
+					if (Math.Pow(ax, 2) != Math.Pow(bx, 2))
+					{
+						Console.WriteLine("es una hiperbola");
+					}
+				}
+			}
+		}
+
+	}
+
+}
+
